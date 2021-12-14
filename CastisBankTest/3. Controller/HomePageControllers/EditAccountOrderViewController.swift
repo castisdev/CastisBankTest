fb2d33dcb37d6689695f11736cec2919c7139ca7
TakJeWon <taknineball@castis.com>
2021-12-14 10:16:19 +0900

    make apply the account order button

--- CastisBankTest/3. Controller/HomePageControllers/EditAccountOrderViewController.swift
+++ CastisBankTest/3. Controller/HomePageControllers/EditAccountOrderViewController.swift
@@ -11,13 +11,14 @@ class EditAccountOrderViewController: UIViewController {
     
     //MARK: - declare instances
     @IBOutlet weak var tableView: UITableView!
-    
-    var fakeModel = AccountModel().mainCellInformation
+    @IBOutlet weak var applyOrderButton: UIButton!
     
     let editCell = SetOrderCell()
     let accountListModel = AccountListModel()
     var receivedAccountList = [AccountList]()
     
+    let uikitFuncs = UIKitFuncs()
+    let colorChip = ColorChip()
     
     //MARK: - VC life cycle
     override func viewDidLoad() {
@@ -27,15 +28,23 @@ class EditAccountOrderViewController: UIViewController {
         tableView.dataSource = self
         
         tableViewSettings()
+        buttonSettings()
+        
+        setConstraints()
 //
 //        accountListModel.delegate = self
 //        accountListModel.getAccountList(user: "test1")
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let homePageViewController = segue.destination as? HomePageViewController else {
+            return print("segue error: edit order -> home page")
+        }
+        homePageViewController.accountList = receivedAccountList
+    }
+    
     private func tableViewSettings(){
-        //inset
-        tableView.frame = view.bounds
-        tableView.frame = tableView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+        //seperate line
         tableView.separatorStyle = .singleLine
         
         //enable scroll
@@ -43,8 +52,29 @@ class EditAccountOrderViewController: UIViewController {
         
         //for editing cell order
         tableView.setEditing(true, animated:false)
-        
-        
+    }
+    
+    private func buttonSettings(){
+        uikitFuncs.buttonSettings(button: applyOrderButton, title: "확인", fontSize: 10, tintColor: .black)
+        applyOrderButton.backgroundColor = colorChip.kakaoYello
+        applyOrderButton.layer.cornerRadius = 10
+    }
+    
+    private func setConstraints(){
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        applyOrderButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            tableView.bottomAnchor.constraint(equalTo: applyOrderButton.topAnchor, constant: 20),
+
+            applyOrderButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            applyOrderButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
+            applyOrderButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            applyOrderButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
 }
@@ -80,18 +110,18 @@ extension EditAccountOrderViewController: UITableViewDelegate, UITableViewDataSo
     
     // MARK: - editing the order of cells (by touch and drop)
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        var target: AccountSpecs? = nil
+        var target: AccountList? = nil
         
         //선택한 인덱스의 모델 삭제해주고
-        target = fakeModel.remove(at: sourceIndexPath.row)
+        target = receivedAccountList.remove(at: sourceIndexPath.row)
         
         //item에 담아서
         guard let item = target else { return }
         
         //가져다놓은 index의 위치에 넣는다.
-        fakeModel.insert(item, at: destinationIndexPath.row)
+        receivedAccountList.insert(item, at: destinationIndexPath.row)
         
-        print(fakeModel)
+        print(receivedAccountList)
         
     }
     
@@ -109,6 +139,8 @@ extension EditAccountOrderViewController: UITableViewDelegate, UITableViewDataSo
         self.navigationController?.popViewController(animated: true)
     }
     
-    
+    @IBAction func pushApplyButton(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
 
