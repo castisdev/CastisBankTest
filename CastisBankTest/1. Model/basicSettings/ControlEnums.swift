4c35c8a39d64e15772dd3ace8db309271f98182e
TakJeWon <taknineball@castis.com>
2021-12-22 14:57:31 +0900

    add annotation(finding last month part)

--- CastisBankTest/1. Model/basicSettings/ControlEnums.swift
+++ CastisBankTest/1. Model/basicSettings/ControlEnums.swift
@@ -43,18 +43,22 @@ class FilterModel{
         }
     }
     
+    // 지난 달 찾는 매소드
     func setSearchLastMonth (now: String) -> [String] {
         
         var lastMonth = ""
         var lastYear = ""
         
+        // 조회하는 시간(accountHistoryListService.now)
         let endDateYear = now.dropFirst(24).prefix(4)
         let endDateMonth = now.dropFirst(4).prefix(3)
+        //받아온 month가 Dec등 이므로 숫자 월로 바꾸는 매소드
         let endDateMonthNum = setMonthToInt(month: String(endDateMonth))
         
-        
+        //for 확인
         print("accepted year and month : ", endDateYear, endDateMonth, ", Int month : ", endDateMonthNum)
         
+        // 1월일 경우 작년 12월 조회, 다른 달은 같은 년도 지난달 조회
         if endDateMonth == "Jan"{
             lastMonth = "12"
             lastYear = String(Int(endDateYear)! - 1)
@@ -63,56 +67,37 @@ class FilterModel{
             lastYear = String(Int(endDateYear)!)
         }
         
+        // 시작일자는 지난달의 1일
         let startDate = lastYear + lastMonth + "01"
-        var endDate = lastYear + lastMonth + "31"
         
+        // 끝나는 일자는 월마다 달라짐. 31일/30일/28일/(윤년)29일
+        var endDate = ""
         switch lastMonth{
-            
+        
+        //31일 까지 있을 때
         case "1","3","5","7","8","10", "12":
             endDate = lastYear + lastMonth + "31"
+        //30일 까지 있을 떄
         case "4", "6", "9", "11":
             endDate = lastYear + lastMonth + "30"
+        // 28일 혹은 29일
         case "2":
             let lastYearInt = Int(lastYear)!
+            //윤년이면 29일
             if lastYearInt % 4 == 0 && lastYearInt % 100 != 0 || lastYearInt % 400 == 0{
                 endDate = lastYear + lastMonth + "29"
             } else {
+                //윤년이 아니면 28일
                 endDate = lastYear + lastMonth + "28"
             }
         default:
             endDate = lastYear + lastMonth + "31"
         }
+        
         return [startDate, endDate]
     }
     
-    func setSearchType(type: String, list: [AccountHistoryList], accountNum: String) -> [AccountHistoryList]{
-        
-        var depositList = [AccountHistoryList]()
-        var withdrawList = [AccountHistoryList]()
-        
-        print("List count : ", list.count)
-        if list.count != 0{
-            for l in 0...(list.count - 1){
-                if list[l].recvAccount == accountNum {
-                    depositList.append(list[l])
-                } else if list[l].sendAccount == accountNum {
-                    withdrawList.append(list[l])
-                }
-            }
-        }
-        
-        switch type{
-        case "전체":
-            return list
-        case "입금만":
-            return depositList
-        case "출금만":
-            return withdrawList
-        default:
-            return list
-        }
-    }
-    
+    //받아온 month가 Dec등 이므로 숫자 월로 바꾸는 매소드
     func setMonthToInt(month: String) -> String{
         
         switch month{
@@ -145,6 +130,34 @@ class FilterModel{
         }
     }
     
+    
+    func setSearchType(type: String, list: [AccountHistoryList], accountNum: String) -> [AccountHistoryList]{
+        
+        var depositList = [AccountHistoryList]()
+        var withdrawList = [AccountHistoryList]()
+        
+        print("List count : ", list.count)
+        if list.count != 0{
+            for l in 0...(list.count - 1){
+                if list[l].recvAccount == accountNum {
+                    depositList.append(list[l])
+                } else if list[l].sendAccount == accountNum {
+                    withdrawList.append(list[l])
+                }
+            }
+        }
+        
+        switch type{
+        case "전체":
+            return list
+        case "입금만":
+            return depositList
+        case "출금만":
+            return withdrawList
+        default:
+            return list
+        }
+    }
     
     func setSearchOrder(order: String, list: [AccountHistoryList]) -> [AccountHistoryList]{
         switch order{
